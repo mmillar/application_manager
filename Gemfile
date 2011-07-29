76dc38f1f568f36f6342fd819752f631cee3e563
Srdjan Pejic <spejic@gmail.com>
2011-07-29 14:22:41 -0400

    Initial setup

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '~>3.0.9'
+gem 'mysql2', '~> 0.2.6'
+gem 'capistrano'
+gem 'capistrano-ext'
+
+# Use unicorn as the web server
+# gem 'unicorn'
+
+group :development, :test do
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ApplicationManager::Application.config.secret_token = '4ff23f8384b22b9e77f8da7ee3d3c69d8c7d286de43e2765cb3237c43ca9880606a8deec6d7a339188e85d67a95994842fe494af8d86f443daa35193b2e55c9c'
