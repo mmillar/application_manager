1c8e1add9c4216caca845c9d1aa52ab49814e5f7
Michael Millar <mmillar@verto.ca>
2011-08-08 11:26:00 -0400

    review submission changes

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -5,6 +5,7 @@ class Admin::ReviewsController < ApplicationController
     
     if @review.update_attributes(params[:review])
       respond_to do |format|
+        format.html { redirect_to admin_profiles_path }
         format.js { render :nothing => true }
       end
     end
