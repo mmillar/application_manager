e5a2672115662a6ad3dfdc22747ea6dd71378949
Srdjan Pejic <spejic@gmail.com>
2011-08-08 16:58:32 -0400

    Copy and configuration changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ ApplicationManager::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.site_host = "torstar.verto.ca"
+  config.site_host = "speakyourmind.thestar.com"
 
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
