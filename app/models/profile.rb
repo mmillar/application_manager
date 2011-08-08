3c56999025c6ac604245a10a1569e002c724c0be
Srdjan Pejic <spejic@gmail.com>
2011-08-08 10:56:17 -0400

    Copy and functionality changes as per Luke's e-mails received on Aug. 8

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,9 +3,9 @@ class Profile < ActiveRecord::Base
   before_create :generate_token
   
   has_one :review, :dependent => :destroy
-  attr_accessible :first_name, :last_name, :email, :phone, :district, :qualifications, :experience, :facebook_url, :facebook_no_of_friends, :twitter_url, :twitter_no_of_followers, :content_sample, :editorial_ideas, :key_issue_1, :key_issue_2, :key_issue_3, :media_formats, :equipment_access, :conflict_of_interest, :other_comments, :accepted_tos, :confirm_email
+  attr_accessible :first_name, :last_name, :email, :phone, :district, :experience, :facebook_url, :facebook_no_of_friends, :twitter_url, :twitter_no_of_followers, :content_sample, :editorial_ideas, :key_issue_1, :key_issue_2, :key_issue_3, :media_formats, :equipment_access, :conflict_of_interest, :other_comments, :accepted_tos, :confirm_email
 
-  validates_presence_of :first_name, :last_name, :email, :phone, :district, :qualifications, :experience, :facebook_url, :facebook_no_of_friends, :twitter_url, :twitter_no_of_followers, :content_sample, :editorial_ideas, :key_issue_1, :key_issue_2, :key_issue_3, :conflict_of_interest, :other_comments, :accepted_tos
+  validates_presence_of :first_name, :last_name, :email, :phone, :district, :experience, :facebook_url, :facebook_no_of_friends, :twitter_url, :twitter_no_of_followers, :content_sample, :editorial_ideas, :key_issue_1, :key_issue_2, :key_issue_3, :conflict_of_interest, :other_comments, :accepted_tos
 
   validates_numericality_of :facebook_no_of_friends, :twitter_no_of_followers
   validates_format_of :phone, :with => /\d*\-*\(*\d{3}\-*\)*\s*\d{3}\-*\d{4}\s?(x|ext\.\s)?\d{0,5}/
